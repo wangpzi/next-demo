a7cef228ecf90dc3e8970bf3b2fba3f6be29768c
王胖子 <wpang520@163.com>
2025-03-06 00:33:36 +0800

    删除配置命令

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: [],
+    domains: [], // 如果你需要允许的图像域名，可以在这里填入
   },
-  output: 'export', // 关键配置，必须有
-  trailingSlash: true, // 避免路由解析错误
+  output: 'export', // 关键配置
+  trailingSlash: true, // 强制路由末尾加斜杠
 };
 
 export default nextConfig;
